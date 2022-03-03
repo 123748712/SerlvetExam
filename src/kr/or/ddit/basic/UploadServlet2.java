25495ccbea7702c935aa8d3c3a92df168b7dc679
123748712 <kemco01@naver.com>
2022-03-03 09:26:12 +0900

    2022.03.03 UploadServlet2 finish

--- src/kr/or/ddit/basic/UploadServlet2.java
+++ src/kr/or/ddit/basic/UploadServlet2.java
@@ -16,28 +16,27 @@ import javax.servlet.http.Part;
 @MultipartConfig(fileSizeThreshold = 1024 * 1024 * 3, maxFileSize = 1024 * 1024 * 40, maxRequestSize = 1024 * 1024 * 50)
 public class UploadServlet2 extends HttpServlet {
 	private static final String UPLOAD_DIR = "upload_files";
-	
-	
+
 //	@Override
 //	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+
 		String uploadPath = getServletContext().getRealPath("/") + File.separator + UPLOAD_DIR;
-		
+
 		File uploadDir = new File(uploadPath);
-		if(!uploadDir.exists()) {
+		if (!uploadDir.exists()) {
 			uploadDir.mkdir();
 		}
 		try {
 			String fileName = "";
-			
-			for(Part part : req.getParts()) {
+
+			for (Part part : req.getParts()) {
 				fileName = getFileName(part);
-				
-				if(fileName != null && !fileName.equals("")) {
+
+				if (fileName != null && !fileName.equals("")) {
 					part.write(uploadPath + File.separator + fileName); // 파일 업로드
 					System.out.println("파일 명 : " + fileName + " 업로드 완료.");
 				}
@@ -48,22 +47,23 @@ public class UploadServlet2 extends HttpServlet {
 		resp.setContentType("text/plan");
 		resp.getWriter().println("sender : " + req.getParameter("sender"));
 		resp.getWriter().println("업로드 완료.");
-		
+
 	}
 
 	// Part객체 파싱하여 파일이름 추출하기
 	// return => 파일명 : 파일명이 존재하지 않으면 null(폼필드)
 	private String getFileName(Part part) {
-		/* 
-			Content-Disposition 헤더
-			
-			2. multipart body를 위한 헤더정보로 사용되는 경우
-			Content-Disposition : form-data
-			Content-Disposition : form-data; name="feildName"
-			Content-Disposition : form-data; name="feildName"; filename="abc.jpg"
+		/*
+		 * Content-Disposition 헤더
+		 * 
+		 * 2. multipart body를 위한 헤더정보로 사용되는 경우 Content-Disposition : form-data
+		 * Content-Disposition : form-data; name="feildName" Content-Disposition :
+		 * form-data; name="feildName"; filename="abc.jpg"
 		 */
-		for(String content : part.getHeader("Content-Disposition").split(";")) {
-			return content.substring(content.indexOf("=") + 1).trim().replace("\"", "");
+		for (String content : part.getHeader("Content-Disposition").split(";")) {
+			if (content.trim().startsWith("filename")) {
+				return content.substring(content.indexOf("=") + 1).trim().replace("\"", "");
+			}
 		}
 		return null;
 	}
